02e1bf3659990d0764386b63ee6a1298589d3b4b
Majin_Vegeta <diogo.azvg@gmail.com>
2023-08-30 12:35:11 -0300

    Add Gem annotate - Run configs annotate.

--- app/models/institution.rb
+++ app/models/institution.rb
@@ -1,5 +1,16 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: institutions
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  image      :jsonb
+#  user_id    :bigint
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Institution < ApplicationRecord
   validates :name, presence: true
   validates :name, uniqueness: { scope: :user_id }
